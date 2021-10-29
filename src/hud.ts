7ee1259ab5e78febc47bf42c73ae0e5d9b590746
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2021-10-30 00:30:52 +0100

    Swap to arcade bitmap font

--- src/hud.ts
+++ src/hud.ts
@@ -2,18 +2,15 @@ import * as Phaser from 'phaser'
 import RexUIPlugin from 'phaser3-rex-plugins/templates/ui/ui-plugin'
 import { SparklerGameEvents } from './constants'
 
-const TEXT_STYLE = {
-  fontFamily: 'VectorBattle',
-  fontSize: '4rem',
-  fontStyle: 'bold',
-  color: 'purple'
-}
+const FONT_KEY = 'arcade'
+const FONT_SIZE = 48
+const FONT_COLOUR = 0x800080
 
 export class HUDScene extends Phaser.Scene {
 
   private rexUI: RexUIPlugin
   private score: number
-  private scoreText: Phaser.GameObjects.Text
+  private scoreText: Phaser.GameObjects.BitmapText
   private gameOverPanel: RexUIPlugin.Sizer
 
   public constructor() {
@@ -24,12 +21,16 @@ export class HUDScene extends Phaser.Scene {
     this.score = 0
   }
 
+  public preload() {
+    this.load.bitmapFont(FONT_KEY, 'assets/fonts/arcade.png', 'assets/fonts/arcade.xml')
+  }
+
   public create() {
     this.game.events.on(SparklerGameEvents.GameStarted, this.onGameStarted, this)
     this.game.events.on(SparklerGameEvents.GameEnded, this.onGameEnded, this)
     this.game.events.on(SparklerGameEvents.ObstacleCleared, this.onObstacleCleared, this)
 
-    this.scoreText = this.add.text(0, 0, '', TEXT_STYLE)
+    this.scoreText = this.add.bitmapText(0, 0, FONT_KEY, '', FONT_SIZE).setTint(FONT_COLOUR)
     this.rexUI.add.sizer({
       orientation: 'horizontal',
       anchor: { left: 'left+20', top: 'top+20' }
@@ -42,8 +43,8 @@ export class HUDScene extends Phaser.Scene {
       anchor: { centerX: 'center', centerY: 'center' },
       space: { item: 100 }
     })
-      .add(this.add.text(0, 0, 'GAME OVER', TEXT_STYLE))
-      .add(this.add.text(0, 0, 'TAP TO RESTART', TEXT_STYLE))
+      .add(this.add.bitmapText(0, 0, FONT_KEY, 'GAME OVER', FONT_SIZE).setTint(FONT_COLOUR))
+      .add(this.add.bitmapText(0, 0, FONT_KEY, 'TAP TO RESTART', FONT_SIZE).setTint(FONT_COLOUR))
       .setVisible(false)
       .layout()
 
